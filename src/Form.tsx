d8fd27afdee1a4676c2e001621707ec08a591e27
Thanga Ganapathy <ganapathy888@gmail.com>
2022-02-21 21:56:57 +0530

    Add parcel bundler for new setup

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,36 +1,3 @@
-import React, { useReducer, forwardRef, useEffect } from 'react';
-
-import FormContext from './formContext';
-import reducer from './reducer';
-import { deepClone } from './util';
-
-interface Props {
-  initialValues?: object;
-  onSubmit: (values: object) => void;
-  children: (formState: object) => React.ReactNode | React.ReactNode;
+export default function Form() {
+  return <div>Form</div>;
 }
-
-export default forwardRef((props: Props, ref: React.Ref<HTMLFormElement>) => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-  // @ts-ignore
-  const [state, dispatch] = useReducer(reducer, deepClone(props.initialValues) || {});
-
-  useEffect(() => {
-    if (props.initialValues) {
-      dispatch({ type: 'SET_STATE', payload: deepClone(props.initialValues) });
-    }
-  }, [props.initialValues]);
-
-  const handleSubmit = (e: React.FormEvent): void => {
-    e.preventDefault();
-    props.onSubmit(state);
-  };
-
-  return (
-    <FormContext.Provider value={{ state, dispatch }}>
-      <form ref={ref} onSubmit={handleSubmit}>
-        {typeof props.children === 'function' ? props.children(state) : props.children}
-      </form>
-    </FormContext.Provider>
-  );
-});
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Form } from './Form';
-export { default as Field } from './Field';
-export { default as FieldArray } from './FieldArray';
-export { default as useField } from './useField';
